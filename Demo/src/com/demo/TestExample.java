899d74b82b7bcdc4814d485327e34f63229d24b7
shivakumarbottu <bottu.shiva.kumar@gmail.com>
2018-10-01 21:04:57 +0530

    code clean up

--- Demo/src/com/demo/TestExample.java
+++ Demo/src/com/demo/TestExample.java
@@ -5,7 +5,6 @@ public class TestExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Execution starts from here");
-System.out.println("Hello Git -- Hub");
 System.out.println("Adding one more statement");
 	}
 
